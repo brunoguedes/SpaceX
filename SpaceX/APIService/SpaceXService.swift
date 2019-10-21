e26855003fa80478e0551b6b7189f819665760aa
Bruno Guedes <bguedes@gmail.com>
2019-10-21 19:31:13 +1000

    Add sorting and filtering controls

--- SpaceX/APIService/SpaceXService.swift
+++ SpaceX/APIService/SpaceXService.swift
@@ -10,29 +10,37 @@ import Foundation
 import RxSwift
 
 protocol SpaceXServiceProtocol {
+    var isLoading: Observable<Bool> { get }
     func getLaunches() -> Observable<Launches>
     func getLaunchDetails(for flightNumber: Int) -> Observable<LaunchDetails>
     func getRocketDetails(for rocketId: String) -> Observable<RocketDetails>
 }
 
 class SpaceXService: SpaceXServiceProtocol {
+    var isLoading: Observable<Bool> {
+        return isLoadingSubject.asObservable()
+    }
     
     let baseService: BaseServiceProtocol
     let baseURL = URL(string: "https://api.spacexdata.com/v3/launches")
+    let isLoadingSubject = BehaviorSubject(value: false)
     
     init(baseService: BaseServiceProtocol = BaseService()) {
         self.baseService = baseService
     }
     
     func getLaunches() -> Observable<Launches> {
+        isLoadingSubject.onNext(true)
         guard let url = URL(string: "launches", relativeTo: baseURL) else {
-            return Observable.create { (observer) -> Disposable in
+            return Observable.create { [weak self] (observer) -> Disposable in
+                self?.isLoadingSubject.onNext(false)
                 observer.onError(BaseServiceError.invalidURL)
                 return Disposables.create()
             }
         }
         let urlRequest = URLRequest(url: url)
-        return baseService.data(request: urlRequest).map { (data) -> Launches in
+        return baseService.data(request: urlRequest).map { [weak self] (data) -> Launches in
+            self?.isLoadingSubject.onNext(false)
             do {
                 let launches = try JSONDecoder().decode(Launches.self, from: data)
                 return launches
@@ -43,14 +51,17 @@ class SpaceXService: SpaceXServiceProtocol {
     }
     
     func getLaunchDetails(for flightNumber: Int) -> Observable<LaunchDetails> {
+        isLoadingSubject.onNext(true)
         guard let url = URL(string: "launches/\(flightNumber)", relativeTo: baseURL) else {
-            return Observable.create { (observer) -> Disposable in
+            return Observable.create { [weak self] (observer) -> Disposable in
+                self?.isLoadingSubject.onNext(false)
                 observer.onError(BaseServiceError.invalidURL)
                 return Disposables.create()
             }
         }
         let urlRequest = URLRequest(url: url)
-        return baseService.data(request: urlRequest).map { (data) -> LaunchDetails in
+        return baseService.data(request: urlRequest).map { [weak self] (data) -> LaunchDetails in
+            self?.isLoadingSubject.onNext(false)
             do {
                 let launchDetails = try JSONDecoder().decode(LaunchDetails.self, from: data)
                 return launchDetails
@@ -61,14 +72,17 @@ class SpaceXService: SpaceXServiceProtocol {
     }
 
     func getRocketDetails(for rocketId: String) -> Observable<RocketDetails> {
+        isLoadingSubject.onNext(true)
         guard let url = URL(string: "rockets/\(rocketId)", relativeTo: baseURL) else {
-            return Observable.create { (observer) -> Disposable in
+            return Observable.create { [weak self] (observer) -> Disposable in
+                self?.isLoadingSubject.onNext(false)
                 observer.onError(BaseServiceError.invalidURL)
                 return Disposables.create()
             }
         }
         let urlRequest = URLRequest(url: url)
-        return baseService.data(request: urlRequest).map { (data) -> RocketDetails in
+        return baseService.data(request: urlRequest).map { [weak self] (data) -> RocketDetails in
+            self?.isLoadingSubject.onNext(false)
             do {
                 let rocketDetails = try JSONDecoder().decode(RocketDetails.self, from: data)
                 return rocketDetails
--- SpaceX/ViewControllers/LaunchesViewController.swift
+++ SpaceX/ViewControllers/LaunchesViewController.swift
@@ -13,19 +13,33 @@ import RxDataSources
 
 class LaunchesViewController: UIViewController {
     
+    private enum Constants {
+        static let title = NSLocalizedString("SpaceX Launches", comment: "LaunchesViewController Title")
+        static let sortByDateText = NSLocalizedString("Sort by Date", comment: "Sort by date segment control text")
+        static let sortByMissionNameText = NSLocalizedString("Sort by Mission Name", comment: "Sort by date segment control text")
+        static let noFilterText = NSLocalizedString("All Launches", comment: "No filter segment control text")
+        static let filterSuccessfulText = NSLocalizedString("Only Successful", comment: "Filter successful segment control text")
+    }
+    
     private let disposeBag = DisposeBag()
+    private let spaceXService: SpaceXService
+    private let controlsView = UIView()
+    private let sortingSegmentedControl = UISegmentedControl(items: [Constants.sortByDateText, Constants.sortByMissionNameText])
+    private let filteringingSegmentedControl = UISegmentedControl(items: [Constants.noFilterText, Constants.filterSuccessfulText])
     private let tableView = UITableView()
+    private let spinnerView = UIActivityIndicatorView(style: .large)
     private let viewModel: LaunchesViewModel
     private let launches: Observable<Launches>
-    private let filterOnlySuccessful = true
+    private let filterOnlySuccessful = false
     private let sortedBy: LaunchesViewModel.SortBy = .date
     
     init(spaceXService: SpaceXService = SpaceXService()) {
+        self.spaceXService = spaceXService
         let launches = spaceXService.getLaunches()
         self.viewModel = LaunchesViewModel(
             launches: launches,
-            onlySuccessful: Observable.from(optional: filterOnlySuccessful),
-            sortedBy: Observable.from(optional: sortedBy)
+            onlySuccessful: filteringingSegmentedControl.rx.selectedSegmentIndex.asObservable().map { return $0 == 0 ? false : true },
+            sortedBy: sortingSegmentedControl.rx.selectedSegmentIndex.asObservable().map { return $0 == 0 ? .date : .missionName }
         )
         self.launches = launches
         super.init(nibName: nil, bundle: nil)
@@ -37,29 +51,72 @@ class LaunchesViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.addPinned(subView: tableView)
+        self.title = Constants.title
         tableView.register(LaunchTableViewCell.self, forCellReuseIdentifier: LaunchTableViewCell.reuseIdentifier)
-        bindRx()
+        
+        setupUI()
+        setupBindings()
     }
     
-    func bindRx() {
+    func setupUI() {
+        let stackView = UIStackView(arrangedSubviews: [controlsView, tableView])
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+        
+        controlsView.backgroundColor = .systemBackground
+        
+        sortingSegmentedControl.selectedSegmentIndex = 0
+        filteringingSegmentedControl.selectedSegmentIndex = 0
+        
+        let controlsStackView = UIStackView(arrangedSubviews: [sortingSegmentedControl, filteringingSegmentedControl])
+        controlsStackView.axis = .vertical
+        controlsStackView.distribution = .equalSpacing
+        controlsStackView.alignment = .center
+        controlsStackView.spacing = .spacing2x
+        controlsStackView.isLayoutMarginsRelativeArrangement = true
+        controlsStackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: .spacing3x, leading: .spacing3x, bottom: .spacing3x, trailing: .spacing3x)
+        controlsStackView.backgroundColor = .systemBackground
+        controlsView.addPinned(subView: controlsStackView)
+        
+        spinnerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(spinnerView)
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            controlsView.widthAnchor.constraint(equalTo: stackView.widthAnchor),
+            tableView.widthAnchor.constraint(equalTo: stackView.widthAnchor),
+            spinnerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            spinnerView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    func setupBindings() {
         let dataSource = RxTableViewSectionedReloadDataSource<SectionOfLaunches>(
-          configureCell: { dataSource, tableView, indexPath, item in
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: LaunchTableViewCell.reuseIdentifier, for: indexPath) as? LaunchTableViewCell else {
-                fatalError()
-            }
-            cell.configure(launchViewModel: item)
-            return cell
+            configureCell: { dataSource, tableView, indexPath, item in
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: LaunchTableViewCell.reuseIdentifier, for: indexPath) as? LaunchTableViewCell else {
+                    fatalError()
+                }
+                cell.configure(launchViewModel: item)
+                return cell
         })
         
         dataSource.titleForHeaderInSection = { dataSource, index in
-          return dataSource.sectionModels[index].header
+            return dataSource.sectionModels[index].header
         }
         
         viewModel.launchesBasicInfoBySection
             .bind(to: tableView.rx.items(dataSource: dataSource))
             .disposed(by: disposeBag)
+        
+        spaceXService.isLoading
+            .bind(to: spinnerView.rx.isAnimating)
+            .disposed(by: disposeBag)
     }
-
+    
 }
